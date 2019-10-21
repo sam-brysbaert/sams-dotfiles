2743f8e950956f756d3ceeb9face227b0428f5ca
sam brysbaert <sam.brysbaert@gmail.com>
2019-10-21 11:28:29 +0200

    added fish config file

---
+++ fish/.config/fish/config.fish
@@ -0,0 +1,14 @@
+# turns the welcome message off
+set fish_greeting
+
+# adds scripts folder to path
+set fish_user_paths $fish_user_paths ~/.scripts
+
+# abbreviations
+abbr s "exec startx" #starts X
+
+# do startx automatically when you login on tty1
+if test (tty) = "/dev/tty1"
+	exec startx
+end
+
